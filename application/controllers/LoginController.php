5665dfc25c97f15c7c8165f265d4c11fa5130bb1
javigd <javigd@garciadorado.j@husky.neu.edu>
2014-04-29 11:12:58 +0200

    CSS Template

---
+++ application/controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+class LoginController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $form = new Application_Form_Register();
+        $this->view->form = $form;
+    }
+
+}
+
